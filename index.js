038819c0e4d52063fc8885344d75cf3075980b1d
ygtw <jjlf23015888@gmail.com>
2014-08-03 04:23:51 +0800

    test port

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ io.on('connection', function(socket){
 
 
 
-http.listen(3000,"0.0.0.0" , function(){
+http.listen((process.env.PORT || 5000),"0.0.0.0" , function(){
   console.log('listening on *:3000');
 });
